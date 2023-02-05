05bacb1dbddb7bef30d0fb30f094124dc804b0cf
Nickolay <nik5te@yandex.ru>
2023-02-05 20:31:33 +0300

    чёт или нечёт

---
+++ task6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка)
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int rem = number % 2;
+if (rem == 0)
+{
+    Console.WriteLine($"Число {number} чётное");
+}
+else
+{
+    Console.WriteLine($"Число {number} нечётное");
+}
