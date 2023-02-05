774e42c7d84b5c0e68527f05c9881dae823a6861
Nickolay <nik5te@yandex.ru>
2023-02-05 20:49:10 +0300

    чётные до N

---
+++ task8/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int A = 2;
+int rem = N % 2;
+if (rem == 0)
+{
+    while (A <= N)
+{
+    Console.Write($"{A} ");
+    A = A + 2;
+}
+}
+else
+{
+    while (A < N)
+{
+    Console.Write($"{A} ");
+    A = A + 2;
+}
+}
