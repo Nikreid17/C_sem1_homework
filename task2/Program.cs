9ac9d8008fb3a1f1d01d8898d2c0560296dc2a87
Nickolay <nik5te@yandex.ru>
2023-02-05 20:06:33 +0300

    большее из двух чисел

---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+Console.Clear();
+Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+if (numberA > numberB)
+{
+    Console.WriteLine($"Первое число {numberA} больше второго {numberB}");
+}
+else
+{
+    Console.WriteLine($"Второе число {numberA} больше первого {numberB}");
+}
